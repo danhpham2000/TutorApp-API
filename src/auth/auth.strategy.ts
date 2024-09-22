5dfe527caba810459b8e0ea94afe7d31a1c626d1
Danh Pham <reepham2020@gmail.com>
2024-09-22 02:33:18 -0500

    Setting up the JWT strategy, add decorator for request, and comments

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -1,4 +1,30 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import * as dotenv from 'dotenv';
+import { Injectable } from '@nestjs/common';
+import { passportJwtSecret } from 'jwks-rsa';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+dotenv.config();
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+      }),
+
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `${process.env.AUTH0_ISSUER_URL}`,
+      algorithms: ['RS256'],
+    });
+  }
+
+  validate(payload: any): any {
+    return payload;
+  }
+}
--- src/tutors/tutors.controller.ts
+++ src/tutors/tutors.controller.ts
@@ -16,27 +16,34 @@ import { TutorDto } from 'src/dto';
 export class TutorsController {
   constructor(private readonly tutorService: TutorsService) {}
 
+  // This endpoints available for all users
   @Get()
   async getAllTutors() {
     return await this.tutorService.getTutors();
   }
 
+  // This end points available for users but only public data, authorized user can have their edit button
   @Get(':id')
   async getSingleTutor(@Param('id') tutorId: string) {
     return await this.tutorService.getTutor(tutorId);
   }
 
+  // This is sign up page for tutor
+  // With Guard
   @Post()
   async createTutor(@Body() tutorDto: TutorDto) {
     return await this.tutorService.createTutor(tutorDto);
   }
 
+  // This is the edit page for tutor
+  // With Guard
   @Put(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateTutor(@Body() tutorDto: TutorDto, @Param('id') tutorId: string) {
     return await this.tutorService.updateTutor(tutorDto, tutorId);
   }
 
+  // This is delete with Guard as well
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteTutor(@Param('id') tutorId: string) {
